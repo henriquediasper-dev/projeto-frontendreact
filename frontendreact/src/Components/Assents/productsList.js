b6abdcc2edeb84e61659a4266969fc5d1c617ba8
henriquedias420 <henriquediasper@hotmail.com>
2023-05-28 15:43:46 -0300

    Finalizei o projeto e-commerce

--- frontendreact/src/Components/Assents/productsList.js
+++ frontendreact/src/Components/Assents/productsList.js
@@ -1,93 +1,89 @@
-import icon1 from "./Images/space1.png"
-import icon2 from "./Images/space2.png"
-import icon3 from "./Images/space3.png"
-import icon4 from "./Images/space4.png"
-import icon5 from "./Images/space5.png"
+import icon1 from "./Images/space1.png";
+import icon2 from "./Images/space2.png";
+import icon3 from "./Images/space3.png";
+import icon4 from "./Images/space4.png";
+import icon5 from "./Images/space5.png";
 
-import icon8 from "./Images/space8.png"
-import icon9 from "./Images/space9.png"
-import icon10 from "./Images/space10.png"
-import icon11 from "./Images/space11.png"
+import icon8 from "./Images/space8.png";
+import icon9 from "./Images/space9.png";
+import icon10 from "./Images/space10.png";
+import icon11 from "./Images/space11.png";
 
-import icon13 from "./Images/space13.png"
-import icon14 from "./Images/space14.png"
-import icon15 from "./Images/space15.png"
-
-
+import icon13 from "./Images/space13.png";
+import icon14 from "./Images/space14.png";
+import icon15 from "./Images/space15.png";
 
 export const productList = [
   {
     id: 1,
     name: "Plutaniano",
     value: 10.0,
-    imageUrl: icon4
+    imageUrl: icon4,
   },
   {
     id: 2,
     name: "Marciano",
     value: 9.0,
-    imageUrl: icon5
-  },  
+    imageUrl: icon5,
+  },
   {
     id: 5,
-    name: "Nárniano",
+    name: "Narniano",
     value: 20.0,
-    imageUrl: icon3
-    },
-    {
-      id: 3,
-      name: "Smoke ET",
-      value: 16.0,
-      imageUrl: icon11
-    },
-    {
-      id: 4,
-      name: "Mustache ET",
-      value: 12.0,
-      imageUrl: icon10
-    },
-    {
-      id: 6,
-      name: "Bad ET",
-      value: 13.0,
-      imageUrl: icon15
-    },
-    {
-      id: 7,
-      name: "Diabão ET",
-      value: 11.0,
-      imageUrl: icon1
-    },
-    {
-      id: 8,
-      name: "Mental ET",
-      value: 15.0,
-      imageUrl: icon9
-    },
-    {
-      id: 9,
-      name: "Robot ET",
-      value: 8.0,
-      imageUrl: icon2
-    },
-    {
-      id: 10,
-      name: "Peace ET",
-      value: 7.0,
-      imageUrl: icon13
-    },
-    {
-      id: 11,
-      name: "Vibes ET",
-      value: 6.0,
-      imageUrl: icon14
-    },
-    {
-      id: 12,
-      name: "Multicolor ET",
-      value: 4.0,
-      imageUrl: icon8
-    },
-  ];
-
-  
+    imageUrl: icon3,
+  },
+  {
+    id: 3,
+    name: "Smoke ET",
+    value: 16.0,
+    imageUrl: icon11,
+  },
+  {
+    id: 4,
+    name: "Mustache ET",
+    value: 12.0,
+    imageUrl: icon10,
+  },
+  {
+    id: 6,
+    name: "Bad ET",
+    value: 13.0,
+    imageUrl: icon15,
+  },
+  {
+    id: 7,
+    name: "Devil ET",
+    value: 11.0,
+    imageUrl: icon1,
+  },
+  {
+    id: 8,
+    name: "Mental ET",
+    value: 15.0,
+    imageUrl: icon9,
+  },
+  {
+    id: 9,
+    name: "Robot ET",
+    value: 8.0,
+    imageUrl: icon2,
+  },
+  {
+    id: 10,
+    name: "Peace ET",
+    value: 7.0,
+    imageUrl: icon13,
+  },
+  {
+    id: 11,
+    name: "Vibes ET",
+    value: 6.0,
+    imageUrl: icon14,
+  },
+  {
+    id: 12,
+    name: "Multicolor ET",
+    value: 4.0,
+    imageUrl: icon8,
+  },
+];
